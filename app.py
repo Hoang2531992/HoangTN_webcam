63342a3dd82a8f8960bfbd32394837d2ec242de8
Hoang2531992 <Hoang2531992@gmail.com>
2023-10-25 14:55:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,3 @@
-import streamlit as st
-from webcam import webcam
+from streamlit_webrtc import webrtc_streamer
 
-captured_image = webcam()
-if captured_image is None:
-    st.write("Waiting for capture...")
-else:
-    st.write("Got an image from the webcam:")
-    st.image(captured_image)
+webrtc_streamer(key="sample")
